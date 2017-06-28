baba9fb2286c8772faed6cce8cf16839eed274f2
Sagar Gaikwad <sagar@allureinc.co>
2017-06-28 21:23:48 +0530

    layout minor changes

--- app/code/local/Allure/MyAccount/Block/Myproducts.php
+++ app/code/local/Allure/MyAccount/Block/Myproducts.php
@@ -10,4 +10,20 @@ class Allure_MyAccount_Block_Myproducts extends Mage_Core_Block_Template
         Mage::app()->getFrontController()->getAction()->getLayout()
         	->getBlock('root')->setHeaderTitle(Mage::helper('myaccount')->__('My Products'));
     }
+    
+    public function getPurchasedItems(){
+    	
+    	$collection = Mage::getResourceModel('sales/order_item_collection')
+    	->addAttributeToSelect('*');
+    	$collection->getSelect()->join( array('orders'=> sales_flat_order),
+    			'orders.entity_id=main_table.order_id',array('orders.customer_email','orders.customer_id'));
+    	
+    	$customer = Mage::getSingleton('customer/session')->getCustomer();
+    	
+    	$collection->addFieldToFilter('customer_id',$customer->getId());
+    	//$collection->getSelect()->group('main_table.product_id');
+    	
+    	return $collection;
+    	//}
+    }
 }
