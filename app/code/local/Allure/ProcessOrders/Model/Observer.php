94cdb142902fe359582d2999e3301e96926884b6
Ajaykumar  Kadam <ajay@allureinc.co>
2017-07-31 19:21:43 +0530

    London Sales Dashboard

--- app/code/local/Allure/ProcessOrders/Model/Observer.php
+++ app/code/local/Allure/ProcessOrders/Model/Observer.php
@@ -1,6 +1,9 @@
 <?php
-require ($_SERVER['DOCUMENT_ROOT'].'/allure/api/process.php');
 class Allure_ProcessOrders_Model_Observer{
+ 
     public function runProcess(){
+        $orderProcessor = new Allure_ProcessOrders_Model_OrderProcessor('https://www.venusbymariatash.com/api/v2_soap?wsdl=1', 'sureshinde', 'sunevenus');
+        $orderProcessor->processOrders();
+        
     }
 }
