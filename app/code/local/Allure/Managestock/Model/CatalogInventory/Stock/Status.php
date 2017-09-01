bb882ccf2d880daf7c5db412d254db3d4e29dff3
Suresh Shinde <suresh@allureinc.co>
2017-09-01 20:30:09 +0530

    Stock Id Updates

--- app/code/local/Allure/Managestock/Model/CatalogInventory/Stock/Status.php
+++ app/code/local/Allure/Managestock/Model/CatalogInventory/Stock/Status.php
@@ -12,15 +12,16 @@ class Allure_Managestock_Model_CatalogInventory_Stock_Status extends Mage_Catalo
      */
     public function assignProduct(Mage_Catalog_Model_Product $product, $stockId = 1, $stockStatus = null)
     {
-    	$manageStockHelper =  Mage::helper('managestock');
+        	$manageStockHelper =  Mage::helper('managestock');
+        
         if (is_null($stockStatus)) {
-        	$websiteId = $manageStockHelper->getWebsiteId();
-        	if(isset($websiteId) && !empty($websiteId)){
-        		//nothing 
-        	}else{
-            	$websiteId = $product->getStore()->getWebsiteId();
-        	}
-        	$stockId = $manageStockHelper->getStockIdByWebsiteId($websiteId);
+            	$websiteId = $manageStockHelper->getWebsiteId();
+            	if(isset($websiteId) && !empty($websiteId)){
+            		//nothing 
+            	}else{
+                	$websiteId = $product->getStore()->getWebsiteId();
+            	}
+            	$stockId = $manageStockHelper->getStockIdByWebsiteId($websiteId);
             //Mage::log($websiteId,Zend_Log::DEBUG,'abc',true);
             $status = $this->getProductStatus($product->getId(), $websiteId, $stockId);
             $stockStatus = isset($status[$product->getId()]) ? $status[$product->getId()] : null;
