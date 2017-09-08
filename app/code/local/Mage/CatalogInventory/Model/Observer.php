f0a534bd443a663a8c98dec810cb6a3c5da6d52f
Suresh Shinde <suresh@allureinc.co>
2017-09-09 05:06:24 +0530

    Ignore

--- app/code/local/Mage/CatalogInventory/Model/Observer.php
+++ app/code/local/Mage/CatalogInventory/Model/Observer.php
@@ -625,7 +625,7 @@ class Mage_CatalogInventory_Model_Observer
 
         // Maybe we've already processed this quote in some event during order placement
         // e.g. call in event 'sales_model_service_quote_submit_before' and later in 'checkout_submit_all_after'
-        if ($quote->getInventoryProcessed()) {
+        if (Mage::registry('allure_posinventory_skipped_date') || $quote->getInventoryProcessed()) {
             return;
         }
         $items = $this->_getProductsQty($quote->getAllItems());
