5c16796141b3fb8dfdb14930bd44653479a6ad3d
Ajaykumar  Kadam <ajay@allureinc.co>
2017-09-09 11:32:21 +0530

    Update taxes for grid

--- allure/removetax.php
+++ allure/removetax.php
@@ -6,6 +6,7 @@ $from = $_GET['from'];
 $to= $_GET['to'];
 $store= $_GET['store'];
 
+
 if(empty($from) || empty($to) || empty($store)){
     die('Please add Store Id ,from & to date limit');
 }
@@ -17,31 +18,32 @@ $orders=Mage::getModel("sales/order")->getCollection()->addAttributeToFilter('st
 ->addAttributeToFilter('created_at', array('from'=>$from, 'to'=>$to))
 ->addAttributeToFilter('status', array('complete','processing','pending','canceled'));
 
-
 foreach ($orders as $order){
     try {
         $order=Mage::getModel('sales/order')->load($order->getEntityId());
         if($order->getBaseTaxAmount() > 0){
-            $order->setBaseTaxInvoiced($order->getBaseTaxInvoiced()-$order->getBaseTaxAmount());
-         
-            
+            if($order->getBaseTaxInvoiced() > 0)
+                $order->setBaseTaxInvoiced($order->getBaseTaxInvoiced()-$order->getBaseTaxAmount());
             $order->setBaseTotalPaid($order->getBaseTotalPaid()-$order->getBaseTaxAmount());
-            $order->setBaseTotalInvoiced($order->getBaseTotalInvoiced()-$order->getBaseTaxAmount());
+            if($order->getBaseTotalInvoiced() >0)
+                $order->setBaseTotalInvoiced($order->getBaseTotalInvoiced()-$order->getBaseTaxAmount());
             $order->setBaseGrandTotal($order->getBaseGrandTotal()-$order->getBaseTaxAmount());
             $order->setBaseSubtotalInclTax($order->getBaseSubtotalInclTax()-$order->getBaseTaxAmount());
             $order->setBaseTaxAmount($order->getBaseTaxAmount()-$order->getBaseTaxAmount());
           
             if($order->getBaseSubtotalCanceled()>0){
                 $order->setBaseSubtotalCanceled($order->getBaseSubtotalCanceled()-$order->getBaseTaxAmount());
-                $order->setBaseTaxCanceled($order->getBaseTaxCanceled()-$order->getBaseTaxAmount());
+                $order->setBaseTaxCanceled(0);
             }
-                
-            $order->setTotalPaid($order->getTotalPaid()-$order->getTaxAmount());
-            $order->setTaxInvoiced($order->getTaxInvoiced()-$order->getTaxAmount());
-            $order->setTotalInvoiced($order->getTotalInvoiced()-$order->getTaxAmount());
+            if($order->getTotalPaid() >0)
+                $order->setTotalPaid($order->getTotalPaid()-$order->getTaxAmount());
+            if($order->getTaxInvoiced() >0)
+                $order->setTaxInvoiced($order->getTaxInvoiced()-$order->getTaxAmount());
+            if($order->getTotalInvoiced() >0)
+                $order->setTotalInvoiced($order->getTotalInvoiced()-$order->getTaxAmount());
             $order->setSubtotalInclTax($order->getSubtotalInclTax()-$order->getTaxAmount());
             $order->setGrandTotal($order->getGrandTotal()-$order->getTaxAmount());
-            $order->setTaxAmount($order->getTaxAmount()-$order->getTaxAmount());
+            $order->setTaxAmount(0);
             
             $items=$order->getAllVisibleItems();
             foreach ($items as $item){
