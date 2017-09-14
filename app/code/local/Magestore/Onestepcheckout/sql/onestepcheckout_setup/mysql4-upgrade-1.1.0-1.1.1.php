f3004e6b77e08c025e7c8f6502e9e382d014fb6c
Suresh Shinde <suresh@allureinc.co>
2017-09-14 20:59:54 +0530

    Remove Plugin Onestep Checkout

--- app/code/local/Magestore/Onestepcheckout/sql/onestepcheckout_setup/mysql4-upgrade-1.1.0-1.1.1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$installer = $this;
-$installer->startSetup();
-
-$installer->run("
-	ALTER TABLE {$this->getTable('sales_flat_order')} ADD `onestepcheckout_order_comment` text NULL
-");
-
-$installer->endSetup(); 
