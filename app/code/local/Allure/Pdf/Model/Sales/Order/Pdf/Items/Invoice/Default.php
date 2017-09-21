ad5b344d6b6502260201e524c20f718993bb0ad0
Sagar Gaikwad <sagar@allureinc.co>
2017-09-21 20:49:07 +0530

    pdf customization

---
+++ app/code/local/Allure/Pdf/Model/Sales/Order/Pdf/Items/Invoice/Default.php
@@ -0,0 +1,148 @@
+<?php
+class Allure_Pdf_Model_Sales_Order_Pdf_Items_Invoice_Default extends Mage_Sales_Model_Order_Pdf_Items_Invoice_Default
+{
+    
+    /**
+     * Draw item line
+     */
+    public function draw()
+    {
+        $order  = $this->getOrder();
+        $item   = $this->getItem();
+        $pdf    = $this->getPdf();
+        $page   = $this->getPage();
+        $lines  = array();
+        
+        // draw Product name
+        $lines[0] = array(array(
+            'text' => Mage::helper('core/string')->str_split($item->getName(), 35, true, true),
+            'feed' => 35,
+        ));
+        
+        // draw SKU
+        $lines[0][] = array(
+            'text'  => Mage::helper('core/string')->str_split($this->getSku($item), 17),
+            'feed'  => 290,
+            'align' => 'right'
+        );
+        
+        // draw QTY
+        $lines[0][] = array(
+            'text'  => $item->getQty() * 1,
+            'feed'  => 435,
+            'align' => 'right'
+        );
+        
+        // draw item Prices
+        $i = 0;
+        $prices = $this->getItemPricesForDisplay();
+        $feedPrice = 395;
+        $feedSubtotal = $feedPrice + 170;
+        foreach ($prices as $priceData){
+            if (isset($priceData['label'])) {
+                // draw Price label
+                $lines[$i][] = array(
+                    'text'  => $priceData['label'],
+                    'feed'  => $feedPrice,
+                    'align' => 'right'
+                );
+                // draw Subtotal label
+                $lines[$i][] = array(
+                    'text'  => $priceData['label'],
+                    'feed'  => $feedSubtotal,
+                    'align' => 'right'
+                );
+                $i++;
+            }
+            // draw Price
+            $lines[$i][] = array(
+                'text'  => $priceData['price'],
+                'feed'  => $feedPrice,
+                'font'  => 'bold',
+                'align' => 'right'
+            );
+            // draw Subtotal
+            $lines[$i][] = array(
+                'text'  => $priceData['subtotal'],
+                'feed'  => $feedSubtotal,
+                'font'  => 'bold',
+                'align' => 'right'
+            );
+            $i++;
+        }
+        
+        // draw Tax
+        $lines[0][] = array(
+            'text'  => $order->formatPriceTxt($item->getTaxAmount()),
+            'feed'  => 495,
+            'font'  => 'bold',
+            'align' => 'right'
+        );
+        
+        // custom options
+        $options = $this->getItemOptions();
+        if ($options) {
+            foreach ($options as $option) {
+                // draw options label
+                
+                $optionStr = $option['label'];
+                if ($option['value']) {
+                    if (isset($option['print_value'])) {
+                        $_printValue = $option['print_value'];
+                    } else {
+                        $_printValue = strip_tags($option['value']);
+                    }
+                }
+                $optionStr = $optionStr ." : ".$_printValue;
+                
+                $lines[][] = array(
+                    'text' => Mage::helper('core/string')->str_split(strip_tags($optionStr), 40, true, true),
+                    'font' => 'italic',
+                    'feed' => 35
+                );
+                
+                //allure comment
+                /* if ($option['value']) {
+                    if (isset($option['print_value'])) {
+                        $_printValue = $option['print_value'];
+                    } else {
+                        $_printValue = strip_tags($option['value']);
+                    }
+                    $values = explode(', ', $_printValue);
+                    foreach ($values as $value) {
+                        $lines[][] = array(
+                            'text' => Mage::helper('core/string')->str_split($value, 30, true, true),
+                            'feed' => 40
+                        );
+                    }
+                } */
+            }
+        }
+        
+        $lineBlock = array(
+            'lines'  => $lines,
+            'height' => 20
+        );
+               
+        $page = $pdf->drawLineBlocks($page, array($lineBlock), array('table_header' => true));
+        $this->setPage($page);
+        
+        //allure code - Start
+        $actionName = Mage::app()->getRequest()->getActionName();
+        if($actionName == "pdfdocs"){
+            $helper    = Mage::helper("allure_pdf");
+            $lineBlockArr = $helper->getItemStockStaus($item , $order ,35);
+            $isShow = $lineBlockArr['is_show'];
+            if($isShow){
+                $lineBlockStockMessage = $lineBlockArr['line_block'];
+                $page = $pdf->drawLineBlocks($page, array($lineBlockStockMessage), array('table_header' => true));
+                $this->setPage($page);
+            }
+            
+            $lineSeparator = $helper->getLineSeparator();
+            $page = $pdf->drawLineBlocks($page, array($lineSeparator), array('table_header' => true));
+            $this->setPage($page);
+        }
+        //allure code - End
+    }
+}
