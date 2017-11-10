4be18e323418da98365f70351f2f67cea09e4703
SureShinde <suresh@allureinc.co>
2017-11-10 20:51:02 +0530

    Format Updates

--- app/code/local/Allure/CurrencyServices/Model/Fixer.php
+++ app/code/local/Allure/CurrencyServices/Model/Fixer.php
@@ -9,16 +9,21 @@ class Allure_CurrencyServices_Model_Fixer extends Mage_Directory_Model_Currency_
         $url = str_replace('{{CURRENCY_TO}}', $currencyTo, $url);
 
         try {
-            sleep(1);
-            $handle = fopen($url, "r");
-            $exchange_data = fread($handle, 2000);
-            fclose($handle);
+            $ch = curl_init();
+            $timeout = 5;
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+            $exchange_data = curl_exec($ch);
+            curl_close($ch);
             
             $exchangeDataObject = json_decode($exchange_data,true);
             
             $exchangeData = $exchangeDataObject['rates'];
             
-            $exchange_rate = $exchangeData[$currencyTo];
+            if (count($exchangeData)) {
+                $exchange_rate = $exchangeData[$currencyFrom.$currencyTo];
+            }
 
             if (!$exchange_rate) {
                 $this->_messages[] = Mage::helper('directory')->__('Cannot retrieve rate from %s', $url);
