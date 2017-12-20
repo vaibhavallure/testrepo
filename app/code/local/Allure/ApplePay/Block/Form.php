651fe454ea38c6200c67e165b308e4ffbc2b320c
SureShinde <suresh@allureinc.co>
2017-12-20 10:30:46 +0530

    Cleanup Phase III

--- app/code/local/Allure/ApplePay/Block/Form.php
+++ app/code/local/Allure/ApplePay/Block/Form.php
@@ -2,9 +2,6 @@
 
 class Allure_ApplePay_Block_Form extends Mage_Payment_Block_Form
 {
-	/**
-	 * Instantiate with ACH payment form.
-	 */
 	protected function _construct()
 	{
 		parent::_construct();
