c0d4c805a589ccbc93b5a635985afd62a7f55bec
Sagar Gaikwad <sagar@allureinc.co>
2017-12-28 14:05:44 +0530

    minor change in teamwork

--- app/code/local/Allure/Teamwork/sql/allure_teamwork_setup/mysql4-install-0.1.0.php
+++ app/code/local/Allure/Teamwork/sql/allure_teamwork_setup/mysql4-install-0.1.0.php
@@ -17,11 +17,12 @@ $installer->run("
          CREATE TABLE IF NOT EXISTS {$this->getTable('allure_teamwork_customer')} (
                 `id` int(11) unsigned NOT NULL auto_increment,
                 `customer_id` int(11) NOT NULL,
+                `email` varchar(255) DEFAULT NULL,
                 `teamwork_customer_id` varchar(255) DEFAULT NULL,
                 `auto_gen_bill_id` varchar(255) DEFAULT NULL,
                 `auto_gen_ship_id` varchar(255) DEFAULT NULL,
                 `is_error` int(11) DEFAULT 0,
-                `error` text,
+                `response` text,
                  PRIMARY KEY  (`id`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
      
