8b9fe15543f36808a15bd98862eac7f229fa7c69
Sagar Gaikwad <sagar@allureinc.co>
2018-02-01 16:58:42 +0530

    teamwork script changes

--- allure/teamwork/cntr_order.php
+++ allure/teamwork/cntr_order.php
@@ -6,10 +6,24 @@ Mage::app()->setCurrentStore(0);
 ini_set('memory_limit', '-1');
 $logFile = "cntr_order_prepare.log";
 
+$page = $_GET["page"];
+$size = $_GET['size'];
+if(empty($page)){
+    die("Please add page");
+}
+
+if(empty($size)){
+    $size = 100;
+}
+
 try{
     $collection = Mage::getModel("sales/order")->getCollection();
     $collection->addFieldToFilter( 'create_order_method', array('eq'=>1));
-    $collection->getSelect()->group('customer_email');
+    //$collection->getSelect()->group('customer_email');
+    $collection->setCurPage($page);
+    $collection->setPageSize($size);
+    $collection->setOrder('entity_id', 'asc');
+    
     Mage::log("count = ".$collection->getSize(),Zend_log::DEBUG,$logFile,true);
     $cnt = 0;
     $resource       = Mage::getSingleton('core/resource');
