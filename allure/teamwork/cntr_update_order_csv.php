e61030763d14b66e4b2a982721f379ad9dd43e18
Sagar Gaikwad <sagar@allureinc.co>
2018-02-02 19:45:36 +0530

    exception handle in tamwork script

--- allure/teamwork/cntr_update_order_csv.php
+++ allure/teamwork/cntr_update_order_csv.php
@@ -24,21 +24,24 @@ try{
     foreach ($collection as $cust){
         $model = Mage::getModel("allure_teamwork/customer")->load($cust->getId());
         $custNo = $model->getCustNo();
-        
-        if(!empty($custNo)){
-            $writeAdapter->update(
-                "sales_flat_order",
-                array("customer_id"  => $model->getCustomerId(),
-                    "customer_email" => $model->getEmail()
-                ),
-                "counterpoint_cust_no = {$custNo}"
-            );
-            Mage::log($cnt." customer_id:".$model->getCustomerId(),Zend_log::DEBUG,$logFile,true);
-        }
-       
-        if (($cnt % 100) == 0) {
-            $writeAdapter->commit();
-            $writeAdapter->beginTransaction();
+        try{
+            if(!empty($custNo)){
+                $writeAdapter->update(
+                    "sales_flat_order",
+                    array("customer_id"  => $model->getCustomerId(),
+                        "customer_email" => $model->getEmail()
+                    ),
+                    "counterpoint_cust_no = {$custNo}"
+                );
+                Mage::log($cnt." customer_id:".$model->getCustomerId(),Zend_log::DEBUG,$logFile,true);
+            }
+           
+            if (($cnt % 100) == 0) {
+                $writeAdapter->commit();
+                $writeAdapter->beginTransaction();
+            }
+        }catch (Exception $e){
+            Mage::log("customer_id:".$model->getCustomerId()." Exception:".$e->getMessage(),Zend_log::DEBUG,$logFile,true);
         }
         $cnt++;
     }
