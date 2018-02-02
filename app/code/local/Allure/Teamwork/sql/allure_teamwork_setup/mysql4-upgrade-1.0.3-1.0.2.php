c72a2c39ce88223130f63a8e446cb504f6e69e29
Sagar Gaikwad <sagar@allureinc.co>
2018-02-02 18:15:58 +0530

    change customer in teamwork

---
+++ app/code/local/Allure/Teamwork/sql/allure_teamwork_setup/mysql4-upgrade-1.0.3-1.0.2.php
@@ -0,0 +1,21 @@
+<?php
+$this->startSetup();
+
+$this->addAttribute('order', 'temp_customer_id', array(
+    'type'          => 'int',
+    'label'         => 'Temp Customer Id',
+    'visible'       => true,
+    'required'      => false,
+    'default'		=> 0
+));
+
+$this->addAttribute('order', 'temp_email', array(
+    'type'          => 'varchar',
+    'label'         => 'Temp Email',
+    'visible'       => true,
+    'required'      => false,
+    'default'		=> null
+));
+
+
+?>
