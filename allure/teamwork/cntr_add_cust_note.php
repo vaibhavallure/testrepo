6e55bc2814055e34cab4e700fb4e97325d1fc0a5
Sagar Gaikwad <sagar@allureinc.co>
2018-02-04 07:42:50 +0530

    minor changes

--- allure/teamwork/cntr_add_cust_note.php
+++ allure/teamwork/cntr_add_cust_note.php
@@ -12,6 +12,11 @@ if(empty($fileName)){
     die("Please specify file name");
 }
 
+$state = $_GET['state'];
+if(empty($state)){
+    die("state mention");
+}
+
 $csv = Mage::getBaseDir('var').DS."export".DS.$fileName;
 try{
     $cnt    = 0;
@@ -29,28 +34,41 @@ try{
         $cust_no = $csvData[0];
         $cust_note   = $csvData[5];
         try{
-            /* $customer    = Mage::getModel("customer/customer")
-                ->load($cust_no,"counterpoint_cust_no"); 
-            if($customer->getId()){
-                $customer->setCustNote($cust_no);
-                $customer->save();
-                Mage::log($cnt." cust_no:".$cust_no,Zend_log::DEBUG,$logFile,true);
-            } */
-            
-            
-            //add cust note from csv
-            $model = Mage::getModel("allure_teamwork/cpcustomer")->load($cust_no,"cust_no");
-            if($model->getId()){
-                $model->setCustNote($cust_note);
-                $model->save();
-                Mage::log($cnt." cust_no:".$cust_no,Zend_log::DEBUG,$logFile,true);
+            if($state == "1"){
+                
+                $customers  = Mage::getModel('customer/customer')
+                ->getCollection()
+                ->addAttributeToSelect('*')
+                ->addAttributeToFilter('counterpoint_cust_no', array('eq' => $custNo));
+                
+                foreach ($customers as $cust){
+                    $customer    = Mage::getModel("customer/customer")
+                    ->load($cust->getId());
+                    if($customer->getId()){
+                        $customer->setCustNote($cust_no);
+                        $customer->save();
+                        Mage::log($cnt." cust_no:".$cust_no." customer_id:".$customer->getId(),Zend_log::DEBUG,$logFile,true);
+                    }
+                    $customer = null;
+                }
+            }
+            elseif($state == "2"){
+            //state ==2
+                //add cust note from csv
+                $model = Mage::getModel("allure_teamwork/cpcustomer")->load($cust_no,"cust_no");
+                if($model->getId()){
+                    $model->setCustNote($cust_note);
+                    $model->save();
+                    Mage::log($cnt." cust_no:".$cust_no,Zend_log::DEBUG,$logFile,true);
+                }
+                $model = null;
             }
             
             if (($cnt % 100) == 0) {
                 $writeAdapter->commit();
                 $writeAdapter->beginTransaction();
             }
-            $model = null;
+            
         }catch (Exception $e){
             Mage::log("cust_no:".$cust_no." exc:".$e->getMessage(),Zend_log::DEBUG,$logFile,true);
         }
