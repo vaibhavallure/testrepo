88ec71ff4c61e286346d7635a1c81e94ea687db1
Sagar Gaikwad <sagar@allureinc.co>
2018-02-04 11:09:29 +0530

    minor change

--- allure/teamwork/cntr_add_cust.php
+++ allure/teamwork/cntr_add_cust.php
@@ -12,24 +12,54 @@ if(empty($fileName)){
     die("Please specify file name");
 }
 
+die;
+
 $csv = Mage::getBaseDir('var').DS."import".DS.$fileName;
 try{
     $cnt    = 0;
-    $io     = new Varien_Io_File();
-    $io->streamOpen($csv, 'r');
+    /* $io     = new Varien_Io_File();
+    $io->streamOpen($csv, 'r'); */
     $resource       = Mage::getSingleton('core/resource');
     
     $writeAdapter   = $resource->getConnection('core_write');
     $writeAdapter->beginTransaction();
     
-    $csvData = $io->streamReadCsv();
-    /* echo "<pre>";
+    /* $csvData = $io->streamReadCsv();
+    echo "<pre>";
     print_r($csvData);die; */
     
     $arrCust = array();
     
-    while($csvData = $io->streamReadCsv()){
+    
+    $csvA = Array();
+    $rowcount = 0;
+    if (($handle = fopen($csv, "r")) !== FALSE) {
+        $max_line_length =  15000;
+        $header = fgetcsv($handle, $max_line_length);
+        foreach($header as $c=>$_cols) {
+            $header[$c] = strtolower(str_replace(" ","_",$_cols));
+        }
+        $header_colcount = count($header);
+        while (($row = fgetcsv($handle, $max_line_length)) !== FALSE) {
+            $row_colcount = count($row);
+            if ($row_colcount == $header_colcount) {
+                $entry = array_combine($header, $row);
+                $csvA[] = $entry;
+            }
+            else {
+                Mage::log("no match",Zend_log::DEBUG,$logFile,true);
+            }
+            $rowcount++;
+        }
+    }
+    
+    /* echo "<pre>";
+    print_r($csvA);die; */
+    
+    
+    /* while($csvData = $io->streamReadCsv()){
         try{
+            
             $custNo = $csvData[0];
             $name  = $csvData[1];
             $fstName = $csvData[3];
@@ -66,9 +96,12 @@ try{
         }catch (Exception $e){
             Mage::log("csv read exc:".$e->getMessage(),Zend_log::DEBUG,$logFile,true);
         }
-    }
+    } */
     
-    foreach ($arrCust as $key=>$value){
+    
+    /* echo "<pre>";
+    print_r($arrCust);die; */
+    foreach ($csvA as $key=>$value){
         try{
             /* if(empty($email1)){
                 if(!empty($email2)){
@@ -87,20 +120,21 @@ try{
             
             $model = Mage::getModel("allure_teamwork/cpcustomer")->load($value['cust_no'],"cust_no");
             if(!$model->getId()){
+                $phone = (!empty($value['phone_1']))?$value['phone_1']:$value['phone_2'];
                 $model->setCustNo($value['cust_no'])
-                ->setEmail($value['email1'])
-                ->setOptionalEmail($value['email2'])
-                ->setName($value['name'])
-                ->setFstName($value['fst_name'])
-                ->setLstName($value['lst_name'])
-                ->setAddr1($value['addr1'])
-                ->setAddr2($value['addr2'])
+                ->setEmail($value['email_adrs_1'])
+                ->setOptionalEmail($value['email_adrs_2'])
+                ->setName($value['nam'])
+                ->setFstName($value['fst_nam'])
+                ->setLstName($value['lst_nam'])
+                ->setAddr1($value['adrs_1'])
+                ->setAddr2($value['adrs_2'])
                 ->setCity($value['city'])
                 ->setState($value['state'])
-                ->setZipCode($value['zip_code'])
-                ->setPhone($value['phone'])
-                ->setCountry($value['country'])
-                ->setGroup($value['group'])
+                ->setZipCode($value['zip_cod'])
+                ->setPhone($phone)
+                ->setCountry($value['cntry'])
+                ->setGroup($value['cust_nam_typ'])
                 ->setStrId($value['str_id'])
                 ->save();
                 Mage::log($cnt." add id:".$model->getId(),Zend_log::DEBUG,$logFile,true);
