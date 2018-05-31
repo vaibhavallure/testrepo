83e83c9babd61a703867ca890e1b31c6cbebb679
Sagar Gaikawad <sagar@aws02.allureinc.local>
2018-05-31 17:29:58 +0530

    facebook functionality

---
+++ app/code/local/Allure/Facebook/Block/Template.php
@@ -0,0 +1,49 @@
+<?php
+
+class Allure_Facebook_Block_Template extends Mage_Core_Block_Template
+{
+	
+	public function isSecure()
+	{
+		return Mage::app()->getStore()->isCurrentlySecure();
+	}
+	
+	public function getConnectUrl()
+	{
+		return $this->getUrl('facebook/customer_account/connect', array('_secure'=>true));
+	}
+	
+	public function getChannelUrl()
+	{
+		return $this->getUrl('facebook/channel', array('_secure'=>$this->isSecure(),'locale'=>$this->getLocale()));
+	}	
+	
+	public function getRequiredPermissions()
+	{
+		return json_encode('email,user_birthday');
+	}
+	
+	public function isEnabled()
+	{
+		return Mage::getSingleton('facebook/config')->isEnabled();
+	}
+	
+	public function getApiKey()
+	{
+		return Mage::getSingleton('facebook/config')->getApiKey();
+	}
+	
+	public function getLocale()
+	{
+		return Mage::getSingleton('facebook/config')->getLocale();
+	}
+	
+    protected function _toHtml()
+    {
+        if (!$this->isEnabled()) {
+            return '';
+        }
+        return parent::_toHtml();
+    }
+	
+}
