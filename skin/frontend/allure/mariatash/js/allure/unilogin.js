9fbe1584c6485afe61ec3ba7fe83128efd3932c8
Sagar Gaikawad <sagar@aws02.allureinc.local>
2018-06-04 20:31:01 +0530

    mobile privacy policy changes

--- skin/frontend/allure/mariatash/js/allure/unilogin.js
+++ skin/frontend/allure/mariatash/js/allure/unilogin.js
@@ -66,6 +66,7 @@ jQuery(document).ready(function(){
 		 var privacyPolicySelector = $j("#popup-register-form #popup_is_privacy_agree");
 		 var isChecked = privacyPolicySelector.prop("checked");
 		 if(isChecked == false){
+			 myForm.validator.validate();
 			 privacyPolicySelector.addClass("checkbox-error-validate");
 			 privacyPolicySelector.parent().addClass("label-error-validate");
 			 return;
