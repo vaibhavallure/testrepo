661e26e0283146154ae77443d6102d1ad9d1e7ef
Sagar Gaikawad <sagar@aws02.allureinc.local>
2018-06-15 15:49:45 +0530

    fb & google count in customer grid

--- app/code/local/Allure/Counterpoint/Block/Adminhtml/Customer/Grid.php
+++ app/code/local/Allure/Counterpoint/Block/Adminhtml/Customer/Grid.php
@@ -152,7 +152,7 @@ class Allure_Counterpoint_Block_Adminhtml_Customer_Grid extends Mage_Adminhtml_B
             ));
         }
 
-  /*       $this->addColumn('fb_login_count', array(
+        $this->addColumn('fb_login_count', array(
         		'header'    => Mage::helper('customer')->__('FB Login Count'),
         		'align'     => 'center',
         		'width'     => '80px',
@@ -164,7 +164,7 @@ class Allure_Counterpoint_Block_Adminhtml_Customer_Grid extends Mage_Adminhtml_B
             'align'     => 'center',
             'width'     => '80px',
             'index'     => 'google_login_count',
-        )); */
+        ));
         
         
         
@@ -176,6 +176,9 @@ class Allure_Counterpoint_Block_Adminhtml_Customer_Grid extends Mage_Adminhtml_B
             'options'   => array(0=>'Website',1=>'CounterPoint',2=>'Sugar CRM',15=>'Facebook',16=>'Google'),
             'index'     => 'customer_type',
         ));
+        
+        
+        
         
         $this->addColumn('action',
             array(
