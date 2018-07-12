54177560a777da7308407d42b41a6dca6124dc6c
Sagar Gaikawad <sagar@aws02.allureinc.local>
2018-07-12 15:25:04 +0530

    update order csv

--- allure/salesforce/updatemage/up_order.php
+++ allure/salesforce/updatemage/up_order.php
@@ -26,21 +26,21 @@ $io             = new Varien_Io_File();
 $io->streamOpen($filepath, 'r');
 
 $salesforceIdIdx       = 0;
-$orderIdIdx            = 3;
+$orderIdIdx            = 1;
 
 $write = $coreResource->getConnection('core_write');
 while($csvData = $io->streamReadCsv()){
     try{
-        $incrementId        = trim($csvData[$orderIdIdx]);
+        $orderId        = trim($csvData[$orderIdIdx]);
         $salesforceId       = trim($csvData[$salesforceIdIdx]);
-        if($incrementId){
-            $order = Mage::getModel("sales/order")->loadByIncrementId($incrementId);
-            if ($order->getId()) {
-                $sql_order = "UPDATE sales_flat_order SET salesforce_order_id='".$salesforceId."' WHERE entity_id ='".$order->getId()."'";
+        if($orderId){
+            //$order = Mage::getModel("sales/order")->load($orderId);
+            if ($orderId) {
+                $sql_order = "UPDATE sales_flat_order SET salesforce_order_id='".$salesforceId."' WHERE entity_id ='".$orderId."'";
                 $write->query($sql_order);
-                Mage::log("order_id:".$incrementId." salesforce_id:".$salesforceId." updated.",Zend_Log::DEBUG,$update_order_log,true);
+                Mage::log("order_id:".$orderId." salesforce_id:".$salesforceId." updated.",Zend_Log::DEBUG,$update_order_log,true);
             }else{
-                Mage::log("order_id:".$incrementId." salesforce_id:".$salesforceId." not updated.",Zend_Log::DEBUG,$update_order_log,true);
+                Mage::log("order_id:".$orderId." salesforce_id:".$salesforceId." not updated.",Zend_Log::DEBUG,$update_order_log,true);
             }
         }
     }catch (Exception $e){
