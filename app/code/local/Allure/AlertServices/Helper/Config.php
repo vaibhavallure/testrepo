a48834bf9dbc298a77c2b341afe0420c092edff2
Farooq Bellard <farooqbellard@allureinc.co>
2018-09-12 14:38:50 +0530

    email for sale fix

--- app/code/local/Allure/AlertServices/Helper/Config.php
+++ app/code/local/Allure/AlertServices/Helper/Config.php
@@ -5,7 +5,7 @@ class Allure_AlertServices_Helper_Config extends Mage_Core_Helper_Abstract
 	const XML_PATH_GROUP_EMAILS  		= 'allure_alert/email_group/group_emails';
 	const XML_PATH_GROUP_EMAILS_NAMES  	= 'allure_alert/email_group/group_names';
 	const XML_PATH_PRODUCT_PRICE_TEMPLATE   = 'allure_alert/email_group/product_price';
-	const XML_PATH_SALE_TEMPLATE	= 'allure_alert/email_group/sale_alert';
+	const XML_PATH_SALE_TEMPLATE	= 'allure_alert/email_group/salealert';
 	const XML_PATH_CHECKOUT_ISSUE_TEMPLATE = 'allure_alert/email_group/checkout_issue';
 
     public function getEmailStatus(){
