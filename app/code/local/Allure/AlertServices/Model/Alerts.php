667a9e9ba7ddcf6f3264ae75a118543db5a395fe
Farooq Bellard <farooqbellard@allureinc.co>
2018-09-18 18:13:33 +0530

    condition fix and cron uncomment

--- app/code/local/Allure/AlertServices/Model/Alerts.php
+++ app/code/local/Allure/AlertServices/Model/Alerts.php
@@ -6,22 +6,22 @@ class Allure_AlertServices_Model_Alerts
     }
 
 	public function alertProductPrice(){
-		try{
-			$helper = Mage::helper('alertservices');
+			try{
+				$helper = Mage::helper('alertservices');
 
-			$status =	$this->getConfigHelper()->getEmailStatus();
-			$collection = Mage::getModel('catalog/product')->getCollection()
-				->addAttributeToSelect('*')
-				->addAttributeToFilter(array(array("attribute"=>"price","eq"=>0)))
-				->addAttributeToFilter(array(array("attribute"=>"status","eq"=>1)));
-				if (count($collection) > 0 && $status) {
-					$helper->sendEmailAlertForProductPrice($collection);
-				}
-			}catch(Exception $e){
-    		Mage::log($e->getMessage(),Zend_log::DEBUG,'allureAlerts.log',true);
-    	}
-			
-	}
+				$status =	$this->getConfigHelper()->getEmailStatus();
+				$collection = Mage::getModel('catalog/product')->getCollection()
+					->addAttributeToSelect('*')
+					->addAttributeToFilter(array(array("attribute"=>"price","eq"=>0)))
+					->addAttributeToFilter(array(array("attribute"=>"status","eq"=>1)));
+					if (count($collection) > 0 && $status) {
+						$helper->sendEmailAlertForProductPrice($collection);
+					}
+				}catch(Exception $e){
+	    		Mage::log($e->getMessage(),Zend_log::DEBUG,'allureAlerts.log',true);
+	    	}
+				
+		}
 
 	public function alertSalesOfFour(){
 		/* Get the collection */
@@ -31,8 +31,8 @@ class Allure_AlertServices_Model_Alerts
 
 			$currdate = Mage::getModel('core/date')->timestamp();
 			$toDate	= date('Y-m-d H:i:s', $currdate);
-			//$fromDate = date('Y-m-d H:i:s', strtotime($toDate) - 60 * 60 * 4);
-			$fromDate = date('Y-m-d H:i:s', strtotime($toDate) - 60 * 15);
+			$fromDate = date('Y-m-d H:i:s', strtotime($toDate) - 60 * 60 * 4);
+			/*$fromDate = date('Y-m-d H:i:s', strtotime($toDate) - 60 * 15);*/
 			$orders = Mage::getModel('sales/order')->getCollection()
 				    ->addFieldToFilter('updated_at', array('from'=>$fromDate, 'to'=>$toDate))
 				    ->addAttributeToFilter('status', array('eq' => Mage_Sales_Model_Order::STATE_COMPLETE))->setOrder('updated_at', 'ASC');
@@ -54,8 +54,8 @@ class Allure_AlertServices_Model_Alerts
 
 			$currdate = Mage::getModel('core/date')->timestamp();
 			$toDate	= date('Y-m-d H:i:s', $currdate);
-			//$fromDate = date('Y-m-d H:i:s', strtotime($toDate) - 60 * 60 * 6);
-			$fromDate = date('Y-m-d H:i:s', strtotime($toDate) - 60 * 15);
+			$fromDate = date('Y-m-d H:i:s', strtotime($toDate) - 60 * 60 * 6);
+			/*$fromDate = date('Y-m-d H:i:s', strtotime($toDate) - 60 * 15);*/
 
 			$orders = Mage::getModel('sales/order')->getCollection()
 				    ->addFieldToFilter('updated_at', array('from'=>$fromDate, 'to'=>$toDate))
@@ -78,15 +78,15 @@ class Allure_AlertServices_Model_Alerts
 
 			$currdate = Mage::getModel('core/date')->timestamp();
 			$toDate	= date('Y-m-d H:i:s', $currdate);
-			//$fromDate = date('Y-m-d H:i:s', strtotime($toDate) - 60 * 60 * 1);
-			$fromDate = date('Y-m-d H:i:s', strtotime($toDate) - 60 * 10);
+			$fromDate = date('Y-m-d H:i:s', strtotime($toDate) - 60 * 60 * 1);
+			/*$fromDate = date('Y-m-d H:i:s', strtotime($toDate) - 60 * 10);*/
 
 			$collection = Mage::getModel('alertservices/issues')->getCollection()
 				->addFieldToFilter('created_at', array('from'=>$fromDate, 'to'=>$toDate))
 				->addFieldToFilter('type',array('eq'=>'checkout'));
 				//echo $collection->getSelect()->__toString();
 				//change count to 10 on live
-				if (count($collection) >= 2 && $status) { 
+				if (count($collection) >= 10 && $status) { 
 					$helper->sendCheckoutIssueAlert($collection);
 				}
 		}catch(Exception $e){
