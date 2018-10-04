59a509dcf5f1d23c07b33ad4071c3b183c1b0c82
Farooq Bellard <farooqbellard@allureinc.co>
2018-10-04 19:45:06 +0530

    alert test file updated for test

--- allure/alertTest.php
+++ allure/alertTest.php
@@ -7,6 +7,7 @@ umask(0);
 $password = $_GET['pass'];*/
 Mage::app('admin');
 
+
 Mage::getModel('alertservices/alerts')->alertAvgPageLoad();
 Mage::getModel('alertservices/alerts')->alertPageNotFound();
 Mage::getModel('alertservices/alerts')->alertNullUsers();
