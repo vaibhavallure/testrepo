e6b4c575a162ae530835e812fa6ff7821839f683
Aditya Gatare <adityag@allureinc.co>
2018-12-12 14:51:41 +0530

    mt-906 backorder time set issue for partial order issue resolved

--- app/code/local/OpsWay/CustomOrderManager/Model/Observer.php
+++ app/code/local/OpsWay/CustomOrderManager/Model/Observer.php
@@ -5,7 +5,7 @@ class OpsWay_CustomOrderManager_Model_Observer {
 
 	    $item = $observer->getQuoteItem();
 	    $product = $observer->getProduct();
-	   	
+
 	   	//$item->setBackorderTime($product->getBackorderTime()); //Comment by Allure
 	    
 	    //Allure Inc, Modification Date:21/06/2017 //AWS12 Date : 15/06/2018
@@ -15,8 +15,8 @@ class OpsWay_CustomOrderManager_Model_Observer {
 	    $stockQty = intval($stockItem->getQty());
 	    $isGiftCard= Mage::helper('amstockstatus')->isGiftcardProduct($product->getSku());
 	    Mage::log('is gift card : '.$isGiftCard,Zend_log::DEBUG,'gift-card.log',true);
-	    
-	    if ($stockQty <= 0 && !$isGiftCard) {
+
+        if (($stockQty <= 0 || $stockQty<$item->getQty()) && !$isGiftCard) {
 	    	if($product->getBackorderTime()){
 	    		$item->setBackorderTime($product->getBackorderTime());
 	    	}else{
