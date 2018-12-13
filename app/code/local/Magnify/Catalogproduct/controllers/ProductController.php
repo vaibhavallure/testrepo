8b9ec720ceea56fca730bb8a1033a7a6b8c27670
Aditya Gatare <adityag@allureinc.co>
2018-12-13 16:30:24 +0530

    defualt instock multi attribute selection completed

--- app/code/local/Magnify/Catalogproduct/controllers/ProductController.php
+++ app/code/local/Magnify/Catalogproduct/controllers/ProductController.php
@@ -125,6 +125,7 @@ class Magnify_Catalogproduct_ProductController extends Mage_Core_Controller_Fron
      */
     public function viewAction()
     {
+
         // Get initial data from request
         $categoryId = (int) $this->getRequest()->getParam('category', false);
         $productId  = (int) $this->getRequest()->getParam('id');
@@ -139,75 +140,185 @@ class Magnify_Catalogproduct_ProductController extends Mage_Core_Controller_Fron
         $selectedColor = $this->getRequest()->getParam("colorOptionId", false);
         $optionHelper = Mage::helper('allure_catalog');
 
+        /*For Default select*/
+        $selectValue1=$selectValue2="";
+        $selAtr1=$selAtr2="";
+        if(!empty($this->getRequest()->getParam("selectValue1", false))) {
+            $selectValue1 = $this->getRequest()->getParam("selectValue1", false);
+        }
+        if(!empty($this->getRequest()->getParam("selectValue2", false))) {
+            $selectValue2 = $this->getRequest()->getParam("selectValue1", false);
+
+        }
+
+
+
+
+
         if (!$selectedColor) {
 
-			$optionId = $this->getRequest()->getParam("optionId", false);
+            $optionId = $this->getRequest()->getParam("optionId", false);
 
             if ($optionId) {
                 $selectedColor = $optionId;
             } else {
-	            $metal = $this->getRequest()->getParam("metal", false);
+                $metal = $this->getRequest()->getParam("metal", false);
 
-	            if ($metal) {
-	                $selectedColor = $optionHelper->getOptionNumber($metal);
-	            }
-			}
+                if ($metal) {
+                    $selectedColor = $optionHelper->getOptionNumber($metal);
+                }
+            }
         }
 
-        if ($selectedColor) {
 
-            $selectedColorText = $optionHelper->getOptionText($selectedColor);
 
-            $resource = Mage::getSingleton('core/resource');
-            $readConnection = $resource->getConnection('core_read');
-            $query="SELECT rel.parent_id,rel.child_id,atr.product_super_attribute_id,atr.attribute_id,itm.qty,cpen.value FROM `catalog_product_relation` rel JOIN catalog_product_super_attribute atr on atr.product_id = rel.parent_id join cataloginventory_stock_item itm on itm.product_id = rel.child_id JOIN catalog_product_entity_int cpen ON (cpen.attribute_id=atr.attribute_id AND cpen.entity_id=rel.child_id) where rel.parent_id = ".$productId." and itm.qty > 0";
-            $results = $readConnection->fetchAll($query);
 
-            if(count($results)>=2)
-            {
-                $cnt=0;
-                foreach ($results as $res) {
+        $core_ses=Mage::getSingleton('core/session');
 
-                    $query = "SELECT attribute_code FROM `eav_attribute` WHERE `attribute_id` = " . $res['attribute_id'];
-                    $results = $readConnection->fetchAll($query);
-                    if ($results[0]['attribute_code'] != 'metal') {
-                        if ($res['value'] != $selectedColor) {
-                            $cnt++;
-                            Mage::log("value : " . $res['value'] . " Selected Color" . $selectedColor, Zend_Log::DEBUG, "abc.log", true);
+        if (!$selectedColor || $core_ses->getData("view_product_id")!=$productId) {
 
-                            if ($cnt == 1) {
-                                $selectValue1 = $res['value'];
-                                $selAtr1 = $res['attribute_id'];
+            $core_ses->setData("view_product_id",$productId);
+            $core_ses->unsetData("selAtr1");
+            $core_ses->unsetData("selectValue1");
+            $core_ses->unsetData("selAtr2");
+            $core_ses->unsetData("selectValue2");
+            $core_ses->unsetData("child");
+
+
+
+            $product = Mage::getModel('catalog/product')->load($productId);
+            if ($product->isConfigurable()) {
+                $productAttributeOptions = $product->getTypeInstance(true)->getConfigurableAttributesAsArray($product);
+                $simpleProducts = $product->getTypeInstance()->getUsedProductCollection()->addAttributeToSelect('sku');
+                $flag=FALSE;
+                foreach ($productAttributeOptions as $productAttribute) {
+
+                    if($productAttribute['attribute_code'] == 'metal'/* 'metal_color' */){
+
+                        foreach ($productAttribute['values'] as $single) {
+
+                            $selectedColorLabel=$single['label'];
+                            $selectedColor = $single['value_index'];
+                            foreach ($simpleProducts as $simple){
+                                $sku=explode('|', $simple->getSku());
+
+                                if(strtolower($selectedColorLabel)==strtolower($sku[1])){
+                                    $stockItem = Mage::getModel('cataloginventory/stock_item')
+                                        ->loadByProduct($simple->getId());
+                                    if($stockItem->getId()){
+                                        if($stockItem->getQty() > 0){
+                                            $selectedColor=$single['value_index'];
+                                            $flag=true;
+                                            //break 2;
+                                            break 2;
+                                        }
+                                    }
+                                }
                             }
-                            if (($cnt >= 2) && ($selectValue1 != $res['value'])) {
-                                $selectValue2 = $res['value'];
-                                $selAtr2 = $res['attribute_id'];
 
+                            if($flag){
                                 break;
                             }
                         }
+
+                        if (!$flag) {
+                            $selectedColor=$productAttribute['values'][0]['value_index'];
+                        }
                     }
                 }
+
+
+
+
+
+
+
+                if ($selectedColor) {
+
+                    $selectedColorText = $optionHelper->getOptionText($selectedColor);
+
+                    $resource = Mage::getSingleton('core/resource');
+                    $readConnection = $resource->getConnection('core_read');
+                    $query="SELECT rel.parent_id,rel.child_id,atr.product_super_attribute_id,atr.attribute_id,itm.qty,cpen.value FROM `catalog_product_relation` rel JOIN catalog_product_super_attribute atr on atr.product_id = rel.parent_id join cataloginventory_stock_item itm on itm.product_id = rel.child_id JOIN catalog_product_entity_int cpen ON (cpen.attribute_id=atr.attribute_id AND cpen.entity_id=rel.child_id) where rel.parent_id = ".$productId." and itm.qty > 0";
+                    $results = $readConnection->fetchAll($query);
+
+                    if(count($results)>=2)
+                    {
+
+
+                        $cnt=0;
+                        foreach ($results as $res) {
+
+                        $childid= $res['child_id'];
+
+
+                            $query = "SELECT attribute_code FROM `eav_attribute` WHERE `attribute_id` = " . $res['attribute_id'];
+                            $results = $readConnection->fetchAll($query);
+                            if ($results[0]['attribute_code'] != 'metal') {
+                                if ($res['value'] != $selectedColor) {
+                                    $cnt++;
+//                                    Mage::log("value : " . $res['value'] . " Selected Color" . $selectedColor, Zend_Log::DEBUG, "abc.log", true);
+
+                                    if ($cnt == 1) {
+                                        $selectValue1 = $res['value'];
+                                        $selAtr1 = $res['attribute_id'];
+                                    }
+                                    if (($cnt >= 2) && ($selectValue1 != $res['value'])) {
+                                        $selectValue2 = $res['value'];
+                                        $selAtr2 = $res['attribute_id'];
+
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+
+                    //Mage::log(" Selected val1 " . $selectValue1, Zend_Log::DEBUG, "abc.log", true);
+
+
+
+                    if($selectValue1 && !$selectValue2)
+                    {
+
+                        $core_ses->setData("selAtr1",$selAtr1);
+                        $core_ses->setData("selectValue1",$selectValue1);
+                        $core_ses->setData("child",$childid);
+
+
+
+                        $core_ses->unsetData("selAtr2");
+                        $core_ses->unsetData("selectValue2");
+
+
+//                        $this->_redirectUrl(rtrim(Mage::getBaseUrl(), '/') . $this->getRequest()->getRequestString() . '?metal=' . $selectedColorText.'&selectValue1='.$selectValue1.'&selAtr1='.$selAtr1.'&child='.$childid);
+                        $this->_redirectUrl(rtrim(Mage::getBaseUrl(), '/') . $this->getRequest()->getRequestString() . '?metal=' . $selectedColorText);
+
+                    }
+                    else if($selectValue1 && $selectValue2)
+                    {
+
+                        $core_ses->setData("selAtr1",$selAtr1);
+                        $core_ses->setData("selectValue1",$selectValue1);
+                        $core_ses->setData("selAtr2",$selAtr2);
+                        $core_ses->setData("selectValue2",$selectValue2);
+                        $core_ses->setData("child",$childid);
+
+//                        $this->_redirectUrl(rtrim(Mage::getBaseUrl(), '/') . $this->getRequest()->getRequestString() . '?metal=' . $selectedColorText.'&selectValue1='.$selectValue1.'&selAtr1='.$selAtr1.'&selectValue2='.$selectValue2.'&selAtr2='.$selAtr2.'&child='.$childid);
+                        $this->_redirectUrl(rtrim(Mage::getBaseUrl(), '/') . $this->getRequest()->getRequestString() . '?metal=' . $selectedColorText);
+
+                    }
+                    else
+                    {
+
+
+                        $this->_redirectUrl(rtrim(Mage::getBaseUrl(), '/') . $this->getRequest()->getRequestString() . '?metal=' . $selectedColorText);
+
+                    }
+                    return;
+                }
             }
-
-
-
-            if($selectValue1 && !$selectValue2)
-            {
-                $this->_redirectUrl(rtrim(Mage::getBaseUrl(), '/') . $this->getRequest()->getRequestString() . '?metal=' . $selectedColorText.'&selectValue1='.$selectValue1.'&selAtr1='.$selAtr1);
-
-            }
-            else if($selectValue1 && $selectValue2)
-            {
-                $this->_redirectUrl(rtrim(Mage::getBaseUrl(), '/') . $this->getRequest()->getRequestString() . '?metal=' . $selectedColorText.'&selectValue1='.$selectValue1.'&selAtr1='.$selAtr1.'&selectValue2='.$selectValue2.'&selAtr2='.$selAtr2);
-
-            }
-            else
-            {
-                $this->_redirectUrl(rtrim(Mage::getBaseUrl(), '/') . $this->getRequest()->getRequestString() . '?metal=' . $selectedColorText);
-
-            }
-            return;
         }
 
         // Render page
@@ -235,9 +346,9 @@ class Magnify_Catalogproduct_ProductController extends Mage_Core_Controller_Fron
             $_id  = (int) $this->getRequest()->getParam('id');
             Mage::helper('catalog/product_view')->prepareAndRender($_id, $this);
             $content = $this->getLayout()
-                            ->createBlock('reports/product_viewed')
-                            ->setTemplate('reports/product_viewed.phtml')
-                            ->toHtml();
+                ->createBlock('reports/product_viewed')
+                ->setTemplate('reports/product_viewed.phtml')
+                ->toHtml();
             $this->getResponse()->setBody($content);
             return;
         } catch (Exception $e) {
@@ -276,3 +387,4 @@ class Magnify_Catalogproduct_ProductController extends Mage_Core_Controller_Fron
         $this->_forward('noRoute');
     }
 }
+
