3019599dd9184d7336e9f071728c43d412acd4b5
Sagar Gaikawad <sagar@aws02.allureinc.local>
2019-01-22 14:24:51 +0000

    block nonemail customer send to remarkety
    (cherry picked from commit 253c81064b2c40d3bf0a20088d6f930ad001ff1d)
    
    (cherry picked from commit 8b96254698112ec97baa48259623c7f7e8a6bee4)

--- app/code/community/Remarkety/Mgconnector/Model/Observer.php
+++ app/code/community/Remarkety/Mgconnector/Model/Observer.php
@@ -73,7 +73,7 @@ class Remarkety_Mgconnector_Model_Observer
     {
         $this->_customer = $observer->getEvent()->getCustomer();
         //block particaular domain customer
-        if(preg_match('/customers.mariatash.com/',$this->_customer->getEmail())){
+        if(preg_match('/customers.mariatash.com/',trim($this->_customer->getEmail()))){
             Mage::log("Blocked Customer Email : ".$this->_customer->getEmail(), Zend_Log::DEBUG, REMARKETY_LOG);
             return $this;
         }
@@ -247,6 +247,15 @@ class Remarkety_Mgconnector_Model_Observer
             $client = new Zend_Http_Client(self::REMARKETY_EVENTS_ENDPOINT, $this->_getRequestConfig($eventType));
             $payload = array_merge($payload, $this->_getPayloadBase($eventType));
             $json = json_encode($payload);
+            
+            //block particaular domain customer
+            $email = $payload["email"];
+            if($email){
+                if(preg_match('/customers.mariatash.com/',trim($email))){
+                    Mage::log("makeRequest Blocked Customer Email : ".$email, Zend_Log::DEBUG, REMARKETY_LOG);
+                    return true;
+                }
+            }
 
             $response = $client
                 ->setHeaders($this->_getHeaders($eventType, $payload))
