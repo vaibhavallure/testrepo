8857a20c5cf6024e011438db7f5968db8136a413
Indrajit Patil <indrajitpatil@allureinc.co>
2019-03-20 01:01:45 +0530

    Phone Number alert removed

--- skin/frontend/base/default/js/allure/jquery.validate.extra.js
+++ skin/frontend/base/default/js/allure/jquery.validate.extra.js
@@ -18,7 +18,6 @@
 
 	$.validator.addMethod( "phone", function( value, element ) {
         value = value.replace(/\s+/g, '');
-        alert(value);
 		return this.optional( element ) || /^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/.test( value );
 	}, "Please specify a valid phone number" );
 
