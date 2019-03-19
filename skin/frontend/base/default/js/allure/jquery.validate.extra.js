c08643b03548f054bc83ae60deab97089b3f5ba4
Indrajit Patil <indrajitpatil@allureinc.co>
2019-03-20 01:00:14 +0530

    Phone Number Space

--- skin/frontend/base/default/js/allure/jquery.validate.extra.js
+++ skin/frontend/base/default/js/allure/jquery.validate.extra.js
@@ -17,7 +17,8 @@
 }(function( $ ) {
 
 	$.validator.addMethod( "phone", function( value, element ) {
-        value = value.replace(/^\s+|\s+$/g, '');
+        value = value.replace(/\s+/g, '');
+        alert(value);
 		return this.optional( element ) || /^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/.test( value );
 	}, "Please specify a valid phone number" );
 
