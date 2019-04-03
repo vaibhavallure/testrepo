e533f959a7c1a186f9fa1d5ff9d1e714a7fbc10f
SureShinde <suresh@allureinc.co>
2019-04-03 12:43:12 +0530

    LA Fix

--- app/code/local/Allure/Appointments/Helper/Data.php
+++ app/code/local/Allure/Appointments/Helper/Data.php
@@ -237,7 +237,7 @@ class Allure_Appointments_Helper_Data extends Mage_Core_Helper_Abstract
 
 		foreach ($timings as $time) {
 			$timeValue = $time['value'];
-			if ("$timeValue" ==  "$value"){
+			if ("$timeValue" >=  "$value"){
 				return $time['label'];
 			}
 		}
