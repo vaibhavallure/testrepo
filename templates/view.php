955c53f0888c12a3f5e40ee1e93150f8e0b11d7f
STANISLAS Pierre-Alexandre <pastanislas@macbook-pro-001.reseau.lan>
2019-10-01 14:16:14 +0200

    Utiliser les librairies et composants installés via composer

--- templates/view.php
+++ templates/view.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <!-- jQuery 2.1.3 -->
-<script src="<?php  echo $this->data['url'];?>js/default/jQuery-2.1.3.min.js"></script>
+<script src="/js/default/jQuery-2.1.3.min.js"></script>
 <!-- <script src="/vendor/almasaeed2010/adminlte/bower_components/jquery/dist/jquery.slim.js"></script> -->
-<!-- Bootstrap 3.3.2 JS -->
-<script src="<?php  echo $this->data['url'];?>js/default/bootstrap.min.js" type="text/javascript"></script>
+<!-- Bootstrap 3.3.7 JS -->
+<script src="/vendor/almasaeed2010/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
 <!-- InputMask -->
-<script src="<?php  echo $this->data['url'];?>js/default/jquery.inputmask.js" type="text/javascript"></script>
-<script src="<?php  echo $this->data['url'];?>js/default/jquery.inputmask.date.extensions.js" type="text/javascript"></script>
-<script src="<?php  echo $this->data['url'];?>js/default/jquery.inputmask.extensions.js" type="text/javascript"></script>
+<script src="/vendor/almasaeed2010/adminlte/plugins/input-mask/jquery.inputmask.js" type="text/javascript"></script>
+<script src="/vendor/almasaeed2010/adminlte/plugins/input-mask/jquery.inputmask.date.extensions.js" type="text/javascript"></script>
+<script src="/vendor/almasaeed2010/adminlte/plugins/input-mask/jquery.inputmask.extensions.js" type="text/javascript"></script>
 <!-- bootstrap date picker -->
-<script src="<?php  echo $this->data['url'];?>js/default/bootstrap-datepicker.js" type="text/javascript" ></script>
-<script src="<?php  echo $this->data['url'];?>js/default/bootstrap-datepicker.fr.js" type="text/javascript" ></script>
+<script src="/vendor/almasaeed2010/adminlte/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js" type="text/javascript" ></script>
+<script src="/vendor/almasaeed2010/adminlte/bower_components/bootstrap-datepicker/dist/locales/bootstrap-datepicker.fr.min.js" type="text/javascript" ></script>
 <!-- bootstrap time picker -->
-<script src="<?php  echo $this->data['url'];?>js/default/bootstrap-timepicker.min.js" type="text/javascript" ></script>
+<script src="/vendor/almasaeed2010/adminlte/bower_components/bootstrap-timepicker/js/bootstrap-timepicker.js" type="text/javascript" ></script>
 <!-- DATA TABES SCRIPT -->
-<script src="<?php  echo $this->data['url'];?>js/default/jquery.dataTables.js" type="text/javascript"></script>
-<script src="<?php  echo $this->data['url'];?>js/default/jquery.dataTables.min.js" type="text/javascript"></script>
-<script src="<?php  echo $this->data['url'];?>js/default/dataTables.bootstrap.js" type="text/javascript"></script>
+<script src="/js/default/jquery.dataTables.js" type="text/javascript"></script>
+<script src="/js/default/jquery.dataTables.min.js" type="text/javascript"></script>
+<script src="/js/default/dataTables.bootstrap.js" type="text/javascript"></script>
 <!-- SlimScroll -->
-<script src="<?php  echo $this->data['url'];?>js/default/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="/vendor/almasaeed2010/adminlte/bower_components/jquery-slimscroll/jquery.slimscroll.js" type="text/javascript"></script>
 <!-- FastClick -->
-<script src='<?php  echo $this->data['url'];?>js/default/fastclick.min.js'></script>
+<script src='/vendor/almasaeed2010/adminlte/bower_components/fastclick/lib/fastclick.js'></script>
 <!-- AdminLTE App -->
-<script src="<?php  echo $this->data['url'];?>/js/default/app.min.js" type="text/javascript"></script>
+<script src="/vendor/almasaeed2010/adminlte/dist/js/adminlte.min.js" type="text/javascript"></script>
 <!-- AdminLTE for demo purposes -->
 <!-- Millesima js campaign -->
-<script src="<?php  echo $this->data['url'];?>js/millesima/campaign.js" type="text/javascript"></script>
-<script src="<?php  echo $this->data['url'];?>js/millesima/message.js" type="text/javascript"></script>
-<script src="<?php  echo $this->data['url'];?>js/millesima/fonctions.js" type="text/javascript"></script>
-<script src="<?php  echo $this->data['url'];?>js/millesima/brief.js" type="text/javascript"></script>
-<script src="<?php  echo $this->data['url'];?>js/millesima/traduction.js" type="text/javascript"></script>
+<script src="/js/millesima/campaign.js" type="text/javascript"></script>
+<script src="/js/millesima/message.js" type="text/javascript"></script>
+<script src="/js/millesima/fonctions.js" type="text/javascript"></script>
+<script src="/js/millesima/brief.js" type="text/javascript"></script>
+<script src="/js/millesima/traduction.js" type="text/javascript"></script>
 
 <!-- Calendrier -->
 <link href='https://unpkg.com/@fullcalendar/core@4.3.1/main.min.css' rel='stylesheet' />
@@ -41,28 +41,28 @@
 <script src='https://unpkg.com/@fullcalendar/list@4.3.0/main.min.js'></script>
 
 <!-- Millesima js fancybox -->
-<script src="<?php  echo $this->data['url'];?>js/default/jquery.fancybox.js"></script>
+<script src="/vendor/fancyapps/fancybox/source/jquery.fancybox.js"></script>
 
 <!-- Ckeditor -->
-<script type="text/javascript" src="<?php  echo $this->data['url'];?>library/ckeditor/ckeditor.js"></script>
+<script type="text/javascript" src="/vendor/ckeditor/ckeditor/ckeditor.js"></script>
 
 <head>
     <meta charset="UTF-8">
     <title>Emailing Application</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- personnalisation css defaut -->
-    <link href="<?php  echo $this->data['url'];?>css/millesima.css" rel="stylesheet" type="text/css" />
-    <!-- Bootstrap 3.3.2 -->
-    <link href="<?php  echo $this->data['url'];?>css/default/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="/css/millesima.css" rel="stylesheet" type="text/css" />
+    <!-- Bootstrap 3.3.7 -->
+    <link href="/vendor/almasaeed2010/adminlte/bower_components/bootstrap/dist/css/bootstrap.css" rel="stylesheet" type="text/css" />
     <!-- Font Awesome Icons -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="/vendor/almasaeed2010/adminlte/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <!-- Ionicons -->
-    <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <link href="/vendor/almasaeed2010/adminlte/bower_components/Ionicons/css/ionicons.min.css" rel="stylesheet" type="text/css" />
     <!-- DATA TABLES -->
-    <link href="<?php  echo $this->data['url'];?>css/default/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="/css/default/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
     <!-- Calendar TABLES -->
-    <link href="<?php  echo $this->data['url'];?>css/default/fullcalendar.min.css" rel="stylesheet" type="text/css" />
-    <link href="<?php  echo $this->data['url'];?>css/default/fullcalendar.print.css" rel="stylesheet" type="text/css" media='print'/>
+    <link href="/css/default/fullcalendar.min.css" rel="stylesheet" type="text/css" />
+    <link href="/css/default/fullcalendar.print.css" rel="stylesheet" type="text/css" media='print'/>
     <!-- Theme style -->
 
 
@@ -74,7 +74,7 @@
     <!-- bootstrap date picker -->
     <link href="/vendor/almasaeed2010/adminlte/bower_components/bootstrap-datepicker/js/bootstrap-datepicker.js" rel="stylesheet" type="text/css" />
     <!-- Millesima fancybox -->
-    <link href="<?php  echo $this->data['url'];?>css/default/jquery.fancybox.css" rel="stylesheet" type="text/css" />
+    <link href="/css/default/jquery.fancybox.css" rel="stylesheet" type="text/css" />
 
 
 
@@ -101,7 +101,7 @@
     <a href="/view/home" class="logo">Selligent</a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top" role="navigation">
-        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
             <span class="sr-only">Menu navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
