b5cda0a04d0a193d15275493f0e75367ff86c77e
Indrajit Patil <indrajitpatil@allureinc.co>
2019-10-11 14:26:11 +0000

    Appointment Grid : Speed Fix
    (cherry picked from commit bc8ea1d0d99558c5645424056ec1af3610e9e68e)

--- app/code/local/Allure/Appointments/Model/Resource/Appointments/Collection.php
+++ app/code/local/Allure/Appointments/Model/Resource/Appointments/Collection.php
@@ -7,18 +7,5 @@ class Allure_Appointments_Model_Resource_Appointments_Collection extends Mage_Co
         parent::_construct();
         $this->_init('appointments/appointments');
     }
-/*OVERRIDED GET SIZE METHOD FOR COUNT OF PIERCING & CHECKUP
-REFRENCE : https://magento.stackexchange.com/questions/5481/issue-using-having-in-magento-collection
-*/
-    public function getSize() {
 
-        if (is_null($this->_totalRecords)) {
-//            $sql = $this->getSelectCountSql(); FOR CUSTOM FILTER COMMENTED THIS CODE
-            $sql = $this->getSelect();
-            $this->_totalRecords = count($this->getConnection()->fetchAll($sql, $this->_bindParams));
-        }
-        return intval($this->_totalRecords);
-    }
-
-    
 }
