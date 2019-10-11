6b5f15629e47c0c870c2e759807ae73cb4462242
nicholas123 <nicholasoliver@allureinc.co>
2019-10-11 17:59:17 +0530

    breadcrumbs adjustment

--- skin/frontend/mariatash/redesign/js/allure/mariatash.js
+++ skin/frontend/mariatash/redesign/js/allure/mariatash.js
@@ -19,15 +19,17 @@ jQuery(document).ready(function() {
 
     if (jQuery(window).width() >= 992){
 
-        var cont_width=jQuery('.myaccount-index-index .mt-myaccount-page').outerWidth();
-        jQuery('.myaccount-index-index .breadcrumbs').css({
+        var cont_width=jQuery('.myaccount-index-index .mt-myaccount-page,.customer-address-index .addresses-list').outerWidth();
+        jQuery('.myaccount-index-index .breadcrumbs,.customer-address-index .breadcrumbs').css({
             'width': cont_width
         });
+
+        
 
         var nav_height=jQuery('header').outerHeight();
         jQuery('.login_security,.my_addresses,.new_address,.category-shop-our-instagram .main-container,.insta-details').css("margin-top",nav_height);
 
-        jQuery('.myaccount-index-index .breadcrumbs').css("margin-top",nav_height/2);
+        jQuery('.myaccount-index-index .breadcrumbs,.customer-account-edit .breadcrumbs,.customer-address-index .breadcrumbs').css("margin-top",nav_height/2);
     }
 
 
@@ -241,8 +243,9 @@ else{
             'width': cont_width
         });
 
-        jQuery('.myaccount-index-index .breadcrumbs').css("margin-top",nav_height/2);
+        jQuery('.myaccount-index-index .breadcrumbs,.customer-account-edit .breadcrumbs').css("margin-top",nav_height/2);
 
+    
 
     var box_width=jQuery('.fs-entry-container').outerWidth();
     jQuery('.fs-entry-container').css('height',box_width);
