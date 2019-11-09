b3a1d9976c1052acba2db9ae415e23da73eb7379
Aditya Gatare <adityag@allureinc.co>
2019-11-09 22:37:53 +0530

    js changes for nav

--- skin/frontend/mariatash/redesign/js/allure/mariatash.js
+++ skin/frontend/mariatash/redesign/js/allure/mariatash.js
@@ -113,15 +113,17 @@ jQuery(document).ready(function () {
                     jQuery(".zopim").addClass("bottom-change");
                 }else{
                     jQuery(".zopim").removeClass("bottom-change");
-                }
+                }debugger
             /*  zopim -code end-----------------*/
 
         }else {
             jQuery('#product-details-flow').removeClass('offset-66');
             jQuery('#product-detail-image').removeClass('fix-image');
         }
-        if (jQuery(window).width() >= 1363) {
 
+
+
+        if (jQuery(window).width() >= 1363) {
             var scroll = jQuery(window).scrollTop();
             if (scroll > jQuery(".mariatash-header").outerHeight()) {
                 jQuery(".mt-logo").addClass('d-none');
@@ -183,6 +185,10 @@ jQuery(document).ready(function () {
             jQuery('.mariatash-header').removeClass('scrolled-menu');
             jQuery("section.sub-menu").removeClass("scrolled");
         }
+
+
+
+
     });
 
 
