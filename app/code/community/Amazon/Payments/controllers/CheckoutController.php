1b86d8c6c6a55d6a12ff354c1b801384f9c90731
Sagar Gaikawad <sagar@aws10.allureinc.dev>
2019-11-23 14:43:35 +0530

    amazon pay logout issue after change payment method fix

--- app/code/community/Amazon/Payments/controllers/CheckoutController.php
+++ app/code/community/Amazon/Payments/controllers/CheckoutController.php
@@ -75,8 +75,8 @@ class Amazon_Payments_CheckoutController extends Amazon_Payments_Controller_Chec
     {
         $this->clearSession();
         //loagout customer if amazon session expired
-        Mage::getSingleton('customer/session')->logout();
-        Mage::getSingleton("core/session")->addError("Amazon login session expired. Please login once again.");
+        /* Mage::getSingleton('customer/session')->logout();
+        Mage::getSingleton("core/session")->addError("Amazon login session expired. Please login once again."); */
         
         $this->_redirect('checkout/cart');
     }
