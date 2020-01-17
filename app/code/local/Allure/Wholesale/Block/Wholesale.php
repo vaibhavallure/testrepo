7962e9360d847c9e4a473a4c7d691af05946c778
Aditya Gatare <adityag@allureinc.co>
2020-01-17 12:47:41 +0530

    wholesale application form update and attachment functionality added new email template

--- app/code/local/Allure/Wholesale/Block/Wholesale.php
+++ app/code/local/Allure/Wholesale/Block/Wholesale.php
@@ -2,5 +2,28 @@
 
 class Allure_Wholesale_Block_Wholesale extends Mage_Core_Block_Template
 {
+    public function application()
+    {
+        $formData=Mage::getSingleton("core/session")->getData('application_data');
+        $data = new Varien_Object();
+        if ($formData) {
+            $data->addData($formData);
+        }
 
+        return $data;
+    }
+    public function getAction()
+    {
+        return Mage::getBaseUrl('web').'wholesale-customer/wholesale/applicationSave';
+    }
+    public function getCountryList()
+    {
+       return Mage::getModel('directory/country')->getResourceCollection()
+            ->loadByStore()
+            ->toOptionArray(true);
+    }
+    public function clearSessionData()
+    {
+        Mage::getSingleton("core/session")->unsetData('application_data');
+    }
 }
