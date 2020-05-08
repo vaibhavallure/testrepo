e2f96e78cb566d64f45a7732d9bd8395462c5ec1
Sagar Gaikawad <sagar@aws10.allureinc.dev>
2020-05-08 15:24:39 +0530

    signyfyd order status change

--- app/code/local/Allure/MultiCheckout/Model/Sales/Order/Payment.php
+++ app/code/local/Allure/MultiCheckout/Model/Sales/Order/Payment.php
@@ -388,19 +388,69 @@ class Allure_MultiCheckout_Model_Sales_Order_Payment extends Mage_Sales_Model_Or
      */
     protected function _authorize($isOnline, $amount)
     {
-        parent::_authorize($isOnline, $amount);
-        $customerId = $this->getOrder()->getCustomerGroupId();
-        /* if($customerId != self::WHOLESALE_CUSTOMER)
-        { */
-            if(Mage::helper("core")->isModuleEnabled("Allure_Orders")){
-                $order = $this->getOrder();
-                $storeId = $order->getStoreId();
-                $isSignifydActive = Mage::helper("allure_orders")->isSignifydActive($storeId);
-                if($isSignifydActive && $order->getState() == Mage_Sales_Model_Order::STATE_PROCESSING){
-                    $order->setState(Mage_Sales_Model_Order::STATE_PENDING_PAYMENT);
-                }
+        // check for authorization amount to be equal to grand total
+        $this->setShouldCloseParentTransaction(false);
+        $isSameCurrency = $this->_isSameCurrency();
+        if (!$isSameCurrency || !$this->_isCaptureFinal($amount)) {
+            $this->setIsFraudDetected(true);
+        }
+        
+        // update totals
+        $amount = $this->_formatAmount($amount, true);
+        $this->setBaseAmountAuthorized($amount);
+        
+        // do authorization
+        $order  = $this->getOrder();
+        $state  = Mage_Sales_Model_Order::STATE_PROCESSING;
+        
+        //signifyd patch
+        if(Mage::helper("core")->isModuleEnabled("Allure_Orders")){
+            $order = $this->getOrder();
+            $storeId = $order->getStoreId();
+            $isSignifydActive = Mage::helper("allure_orders")->isSignifydActive($storeId);
+            
+            $methodInstance = $this->getMethodInstance();
+            $methodInstance->setStore($storeId);
+            $action = $methodInstance->getConfigPaymentAction();
+            
+            if($isSignifydActive && $action == Mage_Payment_Model_Method_Abstract::ACTION_AUTHORIZE){
+                $state = Mage_Sales_Model_Order::STATE_PENDING_PAYMENT;
             }
-        /*}*/
+        }
+        
+        $status = true;
+        if ($isOnline) {
+            // invoke authorization on gateway
+            $this->getMethodInstance()->setStore($order->getStoreId())->authorize($this, $amount);
+        }
+        
+        // similar logic of "payment review" order as in capturing
+        if ($this->getIsTransactionPending()) {
+            $message = Mage::helper('sales')->__('Authorizing amount of %s is pending approval on gateway.', $this->_formatPrice($amount));
+            $state = Mage_Sales_Model_Order::STATE_PAYMENT_REVIEW;
+            if ($this->getIsFraudDetected()) {
+                $status = Mage_Sales_Model_Order::STATUS_FRAUD;
+            }
+        } else {
+            if ($this->getIsFraudDetected()) {
+                $state = Mage_Sales_Model_Order::STATE_PAYMENT_REVIEW;
+                $message = Mage::helper('sales')->__('Order is suspended as its authorizing amount %s is suspected to be fraudulent.', $this->_formatPrice($amount, $this->getCurrencyCode()));
+                $status = Mage_Sales_Model_Order::STATUS_FRAUD;
+            } else {
+                $message = Mage::helper('sales')->__('Authorized amount of %s.', $this->_formatPrice($amount));
+            }
+        }
+        
+        // update transactions, order state and add comments
+        $transaction = $this->_addTransaction(Mage_Sales_Model_Order_Payment_Transaction::TYPE_AUTH);
+        if ($order->isNominal()) {
+            $message = $this->_prependMessage(Mage::helper('sales')->__('Nominal order registered.'));
+        } else {
+            $message = $this->_prependMessage($message);
+            $message = $this->_appendTransactionToMessage($transaction, $message);
+        }
+        $order->setState($state, $status, $message);
+        
         return $this;
     }
 }
