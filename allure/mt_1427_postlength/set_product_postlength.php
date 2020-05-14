f435eb7734e184b75acbe8e82edfea22d7d21ab8
Indrajit Patil <indrajitpatil@allureinc.co>
2020-05-14 13:02:34 +0530

    MT-1427 : Product Update Script

--- allure/mt_1427_postlength/set_product_postlength.php
+++ allure/mt_1427_postlength/set_product_postlength.php
@@ -3,7 +3,20 @@ require_once('../../app/Mage.php');
 umask(0);
 Mage::app();
 Mage::app()->getStore()->setId(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+
 Mage::log('================ START ====================',Zend_Log::DEBUG,'setPostlength.log',true);
+
+$fp = fopen("found_list.txt","a");
+fwrite($fp,'----------- NEW START ------------'.PHP_EOL);
+fclose($fp);
+$fp = fopen("not_found_list.txt","a");
+fwrite($fp,'----------- NEW START ------------'.PHP_EOL);
+fclose($fp);
+$fp = fopen("updated_list.txt","a");
+fwrite($fp,'----------- NEW START ------------'.PHP_EOL);
+fclose($fp);
+
 
 $attribute = Mage::getSingleton('eav/config')
     ->getAttribute(Mage_Catalog_Model_Product::ENTITY, 'default_postlength');
@@ -41,66 +54,83 @@ foreach ($lines as $lineNumber => $line) {
         $data = explode(',', $line);
         /*Data*/
         $plu = isset($data[0])?$data[0]:'';
-        Mage::log('------------------------------------------',Zend_Log::DEBUG,'setPostlength.log',true);
-        Mage::log('PLU :'.$plu,Zend_Log::DEBUG,'setPostlength.log',true);
-        $fiveMM = isset($data[11])?$data[11]:'';
-        $sixPointFiveMM = isset($data[14])?$data[14]:'';
-        $eightMM = isset($data[17])?$data[17]:'';
-        $ninePointFiveMM = isset($data[20])?$data[20]:'';
-        /*Default Postlenth*/
-        if ($data[10] == 'Yes') {
-            $default_postLength = '5MM';
-        } elseif ($data[13] == "Yes") {
-            $default_postLength = '6.5MM';
-        } elseif ($data[16] == "Yes") {
-            $default_postLength = '8MM';
-        } elseif ($data[19] == "Yes") {
-            $default_postLength = '9.5MM';
+
+        $collection = Mage::getModel('catalog/product')->getCollection()
+            ->addAttributeToSelect('*')
+            ->addAttributeToFilter('teamwork_plu', $plu)
+            ->addAttributeToFilter('type_id', 'simple');
+        $foundCount = $collection->getSize();
+
+        Mage::log('PLU '.$plu.' Found Count'.$foundCount,Zend_Log::DEBUG,'setPostlength.log',true);
+        if($foundCount > 0){
+            $totalFound++;
+            $fp = fopen("found_list.txt","a");
+            fwrite($fp,$plu.','.$foundCount.PHP_EOL);
+            fclose($fp);
+        }else{
+            $fp = fopen("not_found_list.txt","a");
+            fwrite($fp,$plu.PHP_EOL);
+            fclose($fp);
+        }
+        $productId = '';
+        if($foundCount == 1) {
+            $product = $collection->getFirstItem();
+            $productId = $product->getId();
+            Mage::log('ID :'.$productId,Zend_Log::DEBUG,'setPostlength.log',true);
         }
 
+        if (!empty($productId)) {
+            $fiveMM = isset($data[11])?$data[11]:'';
+            $sixPointFiveMM = isset($data[14])?$data[14]:'';
+            $eightMM = isset($data[17])?$data[17]:'';
+            $ninePointFiveMM = isset($data[20])?$data[20]:'';
+            /*Default Postlenth*/
+            if ($data[10] == 'Yes') {
+                $default_postLength = '5MM';
+            } elseif ($data[13] == "Yes") {
+                $default_postLength = '6.5MM';
+            } elseif ($data[16] == "Yes") {
+                $default_postLength = '8MM';
+            } elseif ($data[19] == "Yes") {
+                $default_postLength = '9.5MM';
+            }
 
-        if ($plu != '') {
-            $load_product = Mage::getModel('catalog/product')->loadByAttribute('teamwork_plu', $plu);
-            if ($load_product) {
-                $totalFound++;
-                Mage::log('TYPE :'.$load_product->getTypeId(),Zend_Log::DEBUG,'setPostlength.log',true);
-                if ($load_product->getTypeId() == 'simple') {
-                    
-                    $founArray[]=array($plu,$load_product->getTypeId());
-                    $load_product->setFiveMmSku($fiveMM);
-                    $load_product->setSixPointFiveMmSku($sixPointFiveMM);
-                    $load_product->setEightMmSku($eightMM);
-                    $load_product->setNinePointFiveMmSku($ninePointFiveMM);
+            $load_product = Mage::getModel('catalog/product')->load($productId);
+            Mage::log('TYPE :' . $load_product->getTypeId(), Zend_Log::DEBUG, 'setPostlength.log', true);
 
-                    if ($default_postLength != '') {
-                        if (isset($options[$default_postLength])) {
-                            $load_product->setDefaultPostlength($options[$default_postLength]);
-                        }
-                    }
+            $founArray[] = array($plu, $load_product->getTypeId());
+            $load_product->setFiveMmSku($fiveMM);
+            $load_product->setSixPointFiveMmSku($sixPointFiveMM);
+            $load_product->setEightMmSku($eightMM);
+            $load_product->setNinePointFiveMmSku($ninePointFiveMM);
 
-                    try {
-                        Mage::log('Saving',Zend_Log::DEBUG,'setPostlength.log',true);
-                        $totalUpdated++;
-                        /*$load_product->save();*/
-                        Mage::log('Saved',Zend_Log::DEBUG,'setPostlength.log',true);
-                    } catch (Exception $ex) {
-                        Mage::log('Exception while saving :'.$ex->getMessage(),Zend_Log::DEBUG,'setPostlength.log',true);
-                    }
+            if ($default_postLength != '') {
+                if (isset($options[$default_postLength])) {
+                    $load_product->setDefaultPostlength($options[$default_postLength]);
                 }
+            }
 
+            try {
+                Mage::log('Saving', Zend_Log::DEBUG, 'setPostlength.log', true);
+                $totalUpdated++;
+                /*$load_product->save();*/
+                $fp = fopen("updated_list.txt","a");
+                fwrite($fp,$plu.','.$productId.PHP_EOL);
+                fclose($fp);
+                Mage::log('Saved', Zend_Log::DEBUG, 'setPostlength.log', true);
+            } catch (Exception $ex) {
+                Mage::log('Exception while saving :' . $ex->getMessage(), Zend_Log::DEBUG, 'setPostlength.log', true);
             }
-            else{
-                $totalNotFound++;
-                $notFoundArray[] = $plu;
-            }
+
+
 
         }
     }
 }
 
 Mage::log('================ DONE ====================',Zend_Log::DEBUG,'setPostlength.log',true);
-Mage::log('Found Data'.PHP_EOL.json_decode($founArray,true),Zend_Log::DEBUG,'setPostlength.log',true);
+/*Mage::log('Found Data'.PHP_EOL.json_decode($founArray,true),Zend_Log::DEBUG,'setPostlength.log',true);
 Mage::log('Not Found Data'.PHP_EOL.json_decode($notFoundArray,true),Zend_Log::DEBUG,'setPostlength.log',true);
 echo 'Total Found :'.$totalFound.PHP_EOL;
 echo 'Total Not Found :'.$totalNotFound.PHP_EOL;
-echo 'Total Updated :'.$totalUpdated.PHP_EOL;
+echo 'Total Updated :'.$totalUpdated.PHP_EOL;*/
