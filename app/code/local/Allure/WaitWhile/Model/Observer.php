1d567b05bdc2bd160358b824f91c04af1722116f
Sagar Gaikawad <sagar@aws10.allureinc.dev>
2020-07-14 19:52:29 +0530

    wait-while booking integration

---
+++ app/code/local/Allure/WaitWhile/Model/Observer.php
@@ -0,0 +1,183 @@
+<?php 
+
+/**
+ * 
+ * @author allure
+ *
+ */
+class Allure_WaitWhile_Model_Observer
+{
+    protected $is_debug = false;
+    /**
+     * Add wait-while booking logs data about appointment
+     * @param string $message
+     */
+    private function addLog($message)
+    {
+        if($this->is_debug)
+            Mage::log($message,7,"abc.log",true);
+    }
+    /**
+     * @param $observer
+     */
+    public function process($observer)
+    {
+        try{
+            /**@var $helper Allure_WaitWhile_Helper_WClient*/
+            $helper = Mage::helper("allure_waitwhile/wClient");
+            $isDebugLog = $helper->getHelper()->isDebugLog();
+            $this->is_debug = $isDebugLog;
+            
+            if(!$helper->getHelper()->isBookingEnabled()){
+                $this->addLog("Wait-While Booking configuration Disabled.");
+                return ;
+            }
+            $this->addLog("Start Process.");
+            $appointment = $observer->getEvent()->getAppointment();
+            if(!$appointment){
+                $this->addLog("Appointement data not found.");
+                $this->addLog("End Process.");
+                return ;
+            }
+            $appointmentId = $appointment->getId();
+            /**@var $appointment Allure_Appointments_Model_Appointments */
+            $appointment = Mage::getModel('appointments/appointments')->load($appointmentId);
+            
+            $appointmentCustomer = Mage::getModel("appointments/customers")->getCollection()
+            ->addFieldToFilter("appointment_id", $appointmentId)
+            ->getFirstItem();
+            
+            $piercingCode = 1;
+            if($appointmentCustomer->getId()){
+                $piercingCode = $appointmentCustomer->getPiercing();
+            }
+            
+            $waitWhileBookingService = Mage::getModel('allure_waitwhile/services')->getCollection()
+            ->addFieldToFilter("code", $piercingCode)
+            ->getFirstItem();
+            
+            $waitWhileBookingServiceId = "";
+            if($waitWhileBookingService->getId()){
+                $waitWhileBookingServiceId = $waitWhileBookingService->getWaitwhileServiceId();
+            }
+            
+            if(!$waitWhileBookingServiceId){
+                $this->addLog("Wait-While booking service id required.");
+                return ;
+            }
+            
+            $waitWhileBooking = Mage::getModel('allure_waitwhile/booking')->load($appointmentId,"appointment_id");
+            
+            $apptStartTime  = strtotime($appointment->getAppointmentStart());
+            $apptEndTime    = strtotime($appointment->getAppointmentEnd());
+            $duration = ($apptEndTime - $apptStartTime + 60); //duration in seconds
+            $startTime = str_replace("UTC", "T", date("Y-m-dTH:i",$apptStartTime));
+            $endTime = str_replace("UTC", "T", date("Y-m-dTH:i",$apptEndTime+60));
+            $apptStatus = $appointment->getAppStatus();
+            $bookingState = $helper::BOOKING_BOOKED;
+            
+            $waitWhileLocalization = Mage::getModel('allure_waitwhile/localization')->getCollection()->getFirstItem();
+            $waitWhileLocaleId = "";
+            if($waitWhileLocalization->getId()){
+                $waitWhileLocaleId = $waitWhileLocalization->getWaitwhileLocaleId();
+            }
+            
+            if(!$waitWhileLocaleId){
+                $this->addLog("Wait-While localization id required.");
+                return ;
+            }
+            
+            $args = array(
+                "date"=> $startTime,
+                "name"=> $appointment->getFirstname()." ".$appointment->getLastname(),
+                "duration" => $duration,
+                "partySize" => $appointment->getPiercingQty(),
+                "locationId" => $waitWhileLocaleId,
+                "email" => $appointment->getEmail(),
+                "notes" => $appointment->getSpecialNotes(),
+                "externalCustomerId" => null,
+                "externalId" => $appointmentId,
+                "services" => array($waitWhileBookingServiceId => 1),
+                "phone" => $appointment->getPhone(),
+                "state" => $bookingState
+            );
+            
+            $bookingPath = $helper::BOOKING_PATH;
+            
+            if(!$waitWhileBooking->getAppointmentId()){
+                if($apptStatus != $appointment::STATUS_ASSIGNED){
+                    $this->addLog("Appointement status is not ASSIGNED.");
+                    $this->addLog("End Process.");
+                    return ;
+                }
+                
+                //booking availability
+                $bookingAvailability = $helper::BOOKING_AVAILABILITY;
+                $bookingAvailability .= "?locationId={$waitWhileLocaleId}&fromDate={$startTime}&toDate={$endTime}";
+                $availResponse = $helper->processRequest($bookingAvailability,"GET");
+                $availResponse = json_decode($availResponse,true);
+                if(!isset($availResponse['error'])){
+                    $isAvailabilty = false;
+                    foreach ($availResponse as $availability){
+                        $this->addLog("numAvailableSpots = {$availability["numAvailableSpots"]}");
+                        if($availability["numAvailableSpots"] != 0){
+                            $isAvailabilty = true;
+                        }else{
+                            $isAvailabilty = false;
+                            break;
+                        }
+                    }
+                    $this->addLog("Booking Availability = {$isAvailabilty}");
+                    if(!$isAvailabilty){
+                        $bookingState = $helper::BOOKING_WAITING;
+                        $args["state"] = $bookingState;
+                    }
+                }else{
+                    $this->addLog($availResponse);
+                }
+                
+                $response = $helper->processRequest($bookingPath,"POST",$args);
+                $response = json_decode($response,true);
+                if(isset($response["id"])){
+                    $waitWhileBooking->setAppointmentId($appointmentId)
+                    ->setWaitwhileBookingId($response["id"])
+                    ->save();
+                    $this->addLog("Appointment ID: {$appointmentId} booked into wait-while app id : {$response["id"]}");
+                }else{
+                    $this->addLog("Appointment ID: {$appointmentId}. Error while booking in wait-while app.");
+                    $this->addLog($response);
+                }
+            }else{
+                $waitWhileBookingId = $waitWhileBooking->getWaitwhileBookingId();
+                if($apptStatus == $appointment::STATUS_ASSIGNED){
+                    $bookingState = $helper::BOOKING_BOOKED;
+                    $args["state"] = $bookingState;
+                }elseif ($apptStatus == $appointment::STATUS_COMPLETED){
+                    $bookingState = $helper::BOOKING_COMPLETE;
+                    $args["state"] = $bookingState;
+                }elseif ($apptStatus == $appointment::STATUS_CANCELLED){
+                    $bookingState = $helper::BOOKING_COMPLETE;
+                    $args["state"] = $bookingState;
+                    $tag = "CANCELLED";
+                    $args["addTag"]  = $tag;
+                }
+                
+                if($waitWhileBookingId){
+                    $response = $helper->processRequest("{$bookingPath}/{$waitWhileBookingId}","POST",$args);
+                    $response = json_decode($response,true);
+                    if(isset($response["id"])){
+                        $this->addLog("Appointment ID: {$appointmentId} updated into wait-while app id : {$response["id"]}");
+                    }else{
+                        $this->addLog("Appointment ID: {$appointmentId}. Error while booking in wait-while app.");
+                        $this->addLog($response);
+                    }
+                }
+                
+            }
+            
+        }catch (Exception $e){
+            $this->addLog("Exception in ".get_class($this). ".Message: {$e->getMessage()}");
+        }
+        $this->addLog("End Process.");
+    }
+}
---
+++ app/code/local/Allure/WaitWhile/sql/allure_waitwhile_setup/mysql4-install-1.0.0.php
@@ -0,0 +1,32 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+
+$installer->run("
+        CREATE TABLE IF NOT EXISTS {$this->getTable('allure_waitwhile_booking')} (
+            `booking_id` int(11) UNSIGNED NOT NULL auto_increment,
+            `appointment_id` int(11) UNSIGNED NOT NULL,
+            `waitwhile_booking_id` varchar(255) NOT NULL,
+            PRIMARY KEY(`booking_id`),
+            FOREIGN KEY(`appointment_id`) REFERENCES `allure_piercing_appointments`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+        CREATE TABLE IF NOT EXISTS {$this->getTable('allure_waitwhile_services')} (
+            `service_id` smallint(5) UNSIGNED NOT NULL auto_increment,
+            `waitwhile_service_id` varchar(255) NOT NULL,
+            `code` varchar(100) NOT NULL,
+            `name` varchar(200) NOT NULL,
+            `location` varchar(255) DEFAULT NULL,
+            PRIMARY KEY  (`service_id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+        CREATE TABLE IF NOT EXISTS {$this->getTable('allure_waitwhile_localization')} (
+            `locale_id` smallint(5) UNSIGNED NOT NULL auto_increment,
+            `waitwhile_locale_id` varchar(255) NOT NULL,
+            `time_zone` varchar(255) DEFAULT NULL,
+            PRIMARY KEY  (`locale_id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  "); 
+ 
+$installer->endSetup();
+
