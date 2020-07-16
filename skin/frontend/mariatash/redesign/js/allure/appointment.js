293afe56f7268c01eb6f5429388d96510c56ecc0
Aditya Gatare <adityag@allureinc.co>
2020-07-16 13:48:56 +0530

    MT-1484-covid-section-for-appointment 3

--- skin/frontend/mariatash/redesign/js/allure/appointment.js
+++ skin/frontend/mariatash/redesign/js/allure/appointment.js
@@ -753,7 +753,7 @@ var addCovidSection=function (srno) {
 
     });
 
-  var covidSection='<div id="covid-conditions-'+srno+'" class="" data-no="'+srno+'">\n' +
+  var covidSection='<div id="covid-conditions-'+srno+'" class="covid-section" data-no="'+srno+'">\n' +
       '<input type="hidden" name="customer['+srno+'][ans_covid_section]"  value="1">\n'+
       '    <div class="col-12 col-md-12 col-sm-12 col-lg-12">\n' +
       '        <fieldset id="covid-conditions-form">\n' +
