ba0e0ccb896bcf288118ae6a6d5baf89e7738fe1
Indrajeet Latthe <indrajeet183@gmail.com>
2020-09-08 18:09:08 +0530

    added dam api client

--- app/code/local/Allure/TeamworkDam/Helper/Data.php
+++ app/code/local/Allure/TeamworkDam/Helper/Data.php
@@ -1,5 +1,19 @@
 <?php
-class Allure_TeamworkDam_Helper_Data extends Mage_Core_Helper_Abstract
-{
+class Allure_TeamworkDam_Helper_Data extends Mage_Core_Helper_Abstract{
+    const XML_TEAMWORKDAM_STATUS               = "teamworkdam/module_status/module_enabled";
+    const XML_TEAMWORKDAM_URL                  = "teamworkdam/module_status/base_url";
+    const XML_TEAMWORKDAM_ACCESS_TOKEN         = "teamworkdam/module_status/access_token";
 
+
+    public function getTeamworkDamStatus(){
+        return Mage::getStoreConfig(self::XML_TEAMWORKDAM_STATUS);
+    }
+
+    public function getTeamworkDamUrl(){
+        return Mage::getStoreConfig(self::XML_TEAMWORKDAM_URL);
+    }
+
+    public function getTeamworkDamAccessToken(){
+        return Mage::getStoreConfig(self::XML_TEAMWORKDAM_ACCESS_TOKEN);
+    }
 }
