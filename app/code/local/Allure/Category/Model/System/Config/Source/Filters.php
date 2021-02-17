9010d6b65aed5c53ff98073afc5ff8476baf9843
Aditya Gatare <adityag@allureinc.co>
2021-02-17 13:44:22 +0530

    MT-1677 1

--- app/code/local/Allure/Category/Model/System/Config/Source/Filters.php
+++ app/code/local/Allure/Category/Model/System/Config/Source/Filters.php
@@ -6,6 +6,8 @@ class Allure_Category_Model_System_Config_Source_Filters extends Mage_Eav_Model_
         $productAttrs = Mage::getResourceModel('catalog/product_attribute_collection');
         $productAttrs->addFieldToFilter('is_filterable',1);
 
+        $options[]=array('value'=>'','label'=>'None');
+
         foreach ($productAttrs as $_attr){
             $options[] = array(
                 'value' => $_attr->getAttributeCode(),
